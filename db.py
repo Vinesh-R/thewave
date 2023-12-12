1560fa7e007e418090d2ab1f4bad2cf013a4c6da
Vinesh <113302604+Vinesh-R@users.noreply.github.com>
2023-12-12 21:07:10 +0100

    Update db.py

--- db.py
+++ db.py
@@ -3,9 +3,9 @@ import psycopg2.extras
 
 def connect():
   conn = psycopg2.connect(
-    dbname = 'vinesh.rengassamy_db',
+    dbname = '',
     host = 'sqletud.u-pem.fr',
-    password = 'masterhk',
+    password = '',
     cursor_factory = psycopg2.extras.NamedTupleCursor
   )
   conn.autocommit = True
