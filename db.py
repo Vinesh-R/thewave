ede08e592f69c8f4243a5d8a500828598c741a41
xKyonix <113987418+xKyonix@users.noreply.github.com>
2023-12-18 15:05:00 +0100

    Update db.py

--- db.py
+++ db.py
@@ -3,7 +3,7 @@ import psycopg2.extras
 
 def connect():
   conn = psycopg2.connect(
-    dbname = 'mario.lepage_db',
+    dbname = 'lepage_db',
     host = 'sqletud.u-pem.fr',
     password = "09082003Ken",
     cursor_factory = psycopg2.extras.NamedTupleCursor
