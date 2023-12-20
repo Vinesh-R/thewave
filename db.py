8d3cbf83ddfc904dc9acd38c3c8271e84837767a
VINESH <vinesh@pop-os.localdomain>
2023-12-20 17:48:09 +0100

    creation macros navbar et sideboar. Corriger bug suivre groupe.

--- db.py
+++ db.py
@@ -3,9 +3,9 @@ import psycopg2.extras
 
 def connect():
   conn = psycopg2.connect(
-    dbname = 'mario.lepage_db',
-    host = 'sqletud.u-pem.fr',
-    password = "09082003Ken",
+    dbname = 'thewave',
+    host = 'localhost',
+    user = "postgres",
     cursor_factory = psycopg2.extras.NamedTupleCursor
   )
   conn.autocommit = True
