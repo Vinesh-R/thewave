19707720870d2b615fa835461d33c39c1b58c86d
xKyonix <ken.marc83@gmail.com>
2023-12-21 17:49:21 +0100

    creergerer-playlist

--- db.py
+++ db.py
@@ -3,9 +3,10 @@ import psycopg2.extras
 
 def connect():
   conn = psycopg2.connect(
-    dbname = 'mario.lepage_db',
-    host = 'sqletud.u-pem.fr',
-    password = "09082003Ken",
+    dbname = 'thewave',
+    host = 'localhost',
+    user = "postgres",
+    password = "mybdduniv",
     cursor_factory = psycopg2.extras.NamedTupleCursor
   )
   conn.autocommit = True
